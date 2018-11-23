74e60545f77f3e04b6e7021036429b1cc5be66f0
JnxF <paco.7070@hotmail.com>
2018-11-24 01:09:27 +0200

    Mejorando

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import matplotlib.pyplot as plt
 
 base_url = "https://v0wwaqqnpa.execute-api.eu-west-1.amazonaws.com/V1"
 api_key = "8zwOJ3DtNS5QHhs8IKL259i0vjI1OB284zjKAGvr"
@@ -7,22 +9,29 @@ headers = {
     'x-api-key': api_key
 }
 
-def get(url):
-    return requests.get(base_url + url, headers = headers)
-
-def post(url):
-    return requests.post(base_url + url, headers = headers)
-
-def put(url):
-    return requests.put(base_url + url, headers = headers)
-
-def delete(url):
-    return requests.delete(base_url + url)
-
+def parseJSON(request):
+    return json.loads(request.text)
 
 
 def main():
-    print(get("/sites/site_exp").text)
+    times = '''
+        {
+          "timeDateFrom" : "2018-10-10T11:25:00.589234Z",
+          "timeDateTo" : "2018-10-10T12:00:00.589234Z"
+        }
+    '''
+
+    data = parseJSON(requests.post(base_url + "/sites/site_exp/thermalimage", headers=headers, data=times))['data']['items']
+
+    img = data[0]['image']
+
+    print(len(img[2]))
+
+    import numpy as np
+    print(np.matrix(img))
+
+    plt.imshow(img, cmap='cool', annot=True, interpolation='nearest', fmt="d")
+    plt.show()
 
 
 if __name__ == '__main__':
