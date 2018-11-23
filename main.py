678cf4fb458c54243c054e433f5b16e47997141b
JnxF <paco.7070@hotmail.com>
2018-11-24 00:45:09 +0200

    Mejorando

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import requests
+
+base_url = "https://v0wwaqqnpa.execute-api.eu-west-1.amazonaws.com/V1"
+api_key = "8zwOJ3DtNS5QHhs8IKL259i0vjI1OB284zjKAGvr"
+headers = {
+    'accept': 'application/json',
+    'x-api-key': api_key
+}
+
+def get(url):
+    return requests.get(base_url + url, headers = headers)
+
+def post(url):
+    return requests.post(base_url + url, headers = headers)
+
+def put(url):
+    return requests.put(base_url + url, headers = headers)
+
+def delete(url):
+    return requests.delete(base_url + url)
+
+
+
 def main():
-    print("salu2")
+    print(get("/sites/site_exp").text)
+
 
 if __name__ == '__main__':
     main()
