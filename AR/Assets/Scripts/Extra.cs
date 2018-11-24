9b5ad925f7b39e743228deb4e6be9b593b6befcc
JnxF <paco.7070@hotmail.com>
2018-11-25 01:06:35 +0200

    coupones baratos

---
+++ AR/Assets/Scripts/Extra.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Extra : MonoBehaviour {
+
+	private const string exampleCoupon = "51A8GQ3A091";
+
+	void Start() {
+
+	}
+
+	void checkCoupon(string inputCoupon) {
+		if (inputCoupon == exampleCoupon) return true;
+		return false;
+	}
+
+	void Update() {
+
+	}
+}
